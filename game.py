8765bcea8b4f86b3184e1c9ae7ee38f1e697014b
cypher9999 <84623552+cypher9999@users.noreply.github.com>
2021-05-31 22:40:39 +0700

    Add files via upload

--- game.py
+++ game.py
@@ -1,46 +1,22 @@
 import pygame
-from Adventure import Player
+import random
+from Adventure import *
 from camera import Camera
+from menu import Button
 
 pygame.init()
-
-game_State = ["menu", "adventure", "combat"]
 WIDTH, HEIGHT = 800, 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-player = Player(375, 275,50,50,(255,0,0))
-
 run = True
 
 pygame.display.set_caption("Pokemon game")
 
-class Button:
-    def __init__(self, text, x, y, color):
-        self.text = text
-        self.x = x
-        self.y = y
-        self.color = color
-        self.width = 300
-        self.height = 100
-
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-        font = pygame.font.SysFont("comicsansms", 40)
-        text = font.render(self.text, 1, (255,255,255))
-        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2), self.y + round(self.height/2) - round(text.get_height()/2)))
-
-    def click(self, pos):
-        x1 = pos[0]
-        y1 = pos[1]
-        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
-            return True
-        else:
-            return False
-
-btns = [Button("Adventure", 250, 150, (34,139,34)), Button("Combat", 250, 350, (255,0,0))]
-
 class Game():
     def __init__(self):
-        self.state = game_State[0]
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.btns = [Button("Adventure", 250, 150, (34,139,34)), Button("Combat", 250, 350, (255,0,0))]
+        self.game_State = ["menu", "adventure", "combat"]
+        self.state = self.game_State[0]
+        self.ad = Adventure(self.screen)
 
     def menu(self):
         for event in pygame.event.get():
@@ -49,15 +25,15 @@ class Game():
                 pygame.quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-                for btn in btns:
+                for btn in self.btns:
                     if btn.click(pos) and btn.text == "Adventure":
-                        self.state = game_State[1]
+                        self.state = self.game_State[1]
                     #if btn.click(pos) and btn.text == "Combat":
-                        #self.state = game_State[2]
-        for btn in btns:
-            btn.draw(screen)
+                        #self.state = self.game_State[2]
+        for btn in self.btns:
+            btn.draw(self.screen)
         pygame.display.update()
-        screen.fill((211, 211, 211))
+        self.screen.fill((211, 211, 211))
 
 
     def catch(self):
@@ -65,10 +41,15 @@ class Game():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
-        screen.fill((211, 211, 211))
-        player.draw(screen)
-        #player.scroll()
-        player.move()
+        self.screen.fill((211, 211, 211))
+        self.ad.player.draw(self.screen)
+        self.ad.spawn_poke()
+        for pokemon in self.ad.pokemon_list:
+            pokemon.draw(self.screen)
+        self.ad.player.scroll()
+        for pokemon in self.ad.pokemon_list:
+            pokemon.scroll(self.ad.player.offset.x, self.ad.player.offset.y)
+        self.ad.player.move()
         pygame.display.update()
 
 
@@ -79,16 +60,16 @@ class Game():
                 pygame.quit()
 
     def state_manager(self):
-        if self.state == game_State[0]:
+        if self.state == self.game_State[0]:
             self.menu()
-        if self.state == game_State[1]:
+        if self.state == self.game_State[1]:
             self.catch()
+        #if self.state == self.game_State[2]:
+         #   self.combat()
 
-
-game = Game()
-while run:
-    game.state_manager()
-
+g = Game()
+while True:
+    g.state_manager()
 
 
 
